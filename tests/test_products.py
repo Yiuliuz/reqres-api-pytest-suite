e18b28daf766407405f4655857d2fae8b3b0f696
Yiuliuz <villarruelcesarjuliohotmail@gmail.com>
2026-06-15 15:15:00 -0300

    test: expand negative API test coverage

--- tests/test_products.py
+++ tests/test_products.py
@@ -78,6 +78,7 @@ def test_create_product_returns_success_status(
 
 @pytest.mark.write
 @pytest.mark.contract
+@pytest.mark.negative
 @pytest.mark.parametrize(
     "field, value",
     [
@@ -111,17 +112,18 @@ def test_create_product_with_empty_field_returns_error_status(
 
 @pytest.mark.write
 @pytest.mark.contract
+@pytest.mark.negative
 @pytest.mark.parametrize(
     "field , type_try",
     [
         pytest.param("name",123,id="int type name"),
-        pytest.param("name",True,id="list type name"),
-        pytest.param("name",[],id="set type name"),
+        pytest.param("name",True,id="bool type name"),
+        pytest.param("name",[],id="list type name"),
         pytest.param("name",{},id="dict type name"),
         
         pytest.param("price","abc",id="str type price"),
-        pytest.param("price",True,id="list type price"),
-        pytest.param("price",[],id="set type price"),
+        pytest.param("price",True,id="bool type price"),
+        pytest.param("price",[],id="list type price"),
         pytest.param("price",{},id="dict type price")
     ],
 )
@@ -149,6 +151,7 @@ def test_create_product_with_invalid_type_field_returns_error_status(
 
 @pytest.mark.write
 @pytest.mark.contract
+@pytest.mark.negative
 @pytest.mark.parametrize(
     "field",
     [
@@ -176,4 +179,20 @@ def test_create_product_with_missing_field_returns_error_status(
     #THEN response status code is negative
     assert response.status_code == 400, (
         f"Create product with missing {field} should return 400, it returns {response.status_code}"
+    )
+
+
+
+@pytest.mark.read
+@pytest.mark.negative
+def test_get_nonexist_product_returns_404(reqres_client):
+    #GIVEN a configured api client
+
+    #WHEN requests a product by inexistent id
+    response=reqres_client.get_product("INVALID ID")
+
+    #THEN response status code is negative
+    assert response.status_code==404, (
+        f"Request a product by invalid id should return 404, it returns {response.status_code}",
+        f"Text= {response.text}"
     )
